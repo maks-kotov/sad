96340cc695456fea3eef7f6e371a70d58a5825fb
maksonLinux <makson@laptop626>
2025-03-15 18:53:19 +0500

    стилизация готова выведенных сортов готова

--- scripts/main.js
+++ scripts/main.js
@@ -11,40 +11,40 @@ titles.forEach(el => {
     
     resultsList.insertAdjacentHTML('beforeend', result)
 })
-resultsList.style.display="block"
-// input.addEventListener('input', function() {
-//     let value = this.value.trim().toLowerCase()
+// resultsList.style.display="block"
+input.addEventListener('input', function() {
+    let value = this.value.trim().toLowerCase()
     
-//     if(value !== '') {
+    if(value !== '') {
 
 
-//    for (const el of resultsList.children) {    
+   for (const el of resultsList.children) {    
         
-//         const title = el.children[0] // title - это тег li, вложенный в a, (содержит название сорта)
+        const title = el.children[0] // title - это тег li, вложенный в a, (содержит название сорта)
         
-//         if(title.textContent.toLowerCase().search(value) === -1) {
-//             title.innerHTML = el.textContent   //по факту это можно и не ставить
-//             title.classList.add('hide')     
-//         }
-//         else {
-//             let str = title.textContent
-//             title.innerHTML=insertMark(str, el.textContent.toLowerCase().search(value), value.length)
-//         }
-//    }
+        if(title.textContent.toLowerCase().search(value) === -1) {
+            title.innerHTML = el.textContent   //по факту это можно и не ставить
+            title.classList.add('hide')     
+        }
+        else {
+            let str = title.textContent
+            title.innerHTML=insertMark(str, el.textContent.toLowerCase().search(value), value.length)
+        }
+   }
 
-//             resultsList.style.display="block"
+            resultsList.style.display="block"
 
-//     }
-//     else {
-//        for (const el of resultsList.children) {
-//         const title = el.children[0] // title - это тег li, вложенный в a, (содержит название сорта)
-//         title.classList.remove('hide')
-//         title.innerHTML = el.textContent
-//        }
-//        resultsList.style.display="none"
-//     }
-// })
+    }
+    else {
+       for (const el of resultsList.children) {
+        const title = el.children[0] // title - это тег li, вложенный в a, (содержит название сорта)
+        title.classList.remove('hide')
+        title.innerHTML = el.textContent
+       }
+       resultsList.style.display="none"
+    }
+})
 
-// function insertMark(string, pos, len) {
-//     return string.slice(0, pos) + '<mark>' + string.slice(pos, pos+len) + '</mark>' + string.slice(pos+len, string.length) 
-// }
+function insertMark(string, pos, len) {
+    return string.slice(0, pos) + '<mark>' + string.slice(pos, pos+len) + '</mark>' + string.slice(pos+len, string.length) 
+}
